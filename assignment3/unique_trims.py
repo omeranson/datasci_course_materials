5ea6125b712d296d8fdff588ba9a51a19a43a01f
Omer Anson <oaanson@gmail.com>
2018-02-15 15:42:06 +0200

    Assignment 3 / problem 5

---
+++ assignment3/unique_trims.py
@@ -0,0 +1,28 @@
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    id_ = record[0]
+    value = record[1]
+    trimmed = value[:-10]
+    mr.emit_intermediate(trimmed, id_)
+
+def reducer(key, list_of_values):
+    mr.emit(key)
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
